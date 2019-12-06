352428c1d579fa4e08ebe1229b44bfe243ecf4a1
gleb7raul <glebimptab@gmail.com>
2019-12-06 21:27:21 +0200

    change: task_1.2 refactoring

--- task1/src/csvreader.js
+++ task1/src/csvreader.js
@@ -1,4 +1,6 @@
 const csvFilePath = './data/readFile.csv';
+const outputForTxt = './resultsData/csvToTxt.txt';
+const outputForJson = '../csvToJson.json';
 const csv = require('csvtojson');
 const csvParser = require('csv-parser');
 const fs = require('fs');
@@ -7,13 +9,22 @@ class CSVreader {
     constructor(){}
     init() {
         try {
+            this.checkingFileAvailability();
             this.readCSV();
+            
+            //file has been saved 1 level above the root folder
             this.csvToJsonObj();
-            this.csvToTxt();
-           // this.lineParser();
+            //file has been saved in resultsData folder
+            this.csvToTxtWithLineParse();
         } catch {
             this.handleError();
         };
+    }
+
+    checkingFileAvailability() {
+        fs.access(csvFilePath, fs.constants.F_OK, (err) => {
+            console.log(`Our file of path:${csvFilePath} ${err ? 'does not exist' : 'exists'}`);
+          });
     }
 
     readCSV() {
@@ -36,29 +47,30 @@ class CSVreader {
         csv()
         .fromFile(csvFilePath)
         .then((jsonObj)=>{
-            console.log(jsonObj);
+            let json = JSON.stringify(jsonObj);
+            fs.writeFile(outputForJson, json, (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+              });
         });
     }
 
-    csvToTxt() {
+    csvToTxtWithLineParse() {
         csv({
+            noheader: false,
+            headers: ['book','author','Amount','price'],
+            output: "json",
             ignoreColumns: /(Amount)/,
             checkType: true
         })
         .fromFile(csvFilePath)
-        .then((jsonObj)=>{
-            const newFile = JSON.stringify(jsonObj);
-            fs.writeFileSync('test.txt', newFile);
-            console.log(newFile);
-        });
-    }
-
-    lineParser() {
-        csv()
-        .preRawData((csvFilePath)=>{
-            var newData=csvRawData.replace('some value','another value');
-            console.log(newData);
-            return newData;
+        .subscribe((jsonObjLine)=>{
+            const newLine = JSON.stringify(jsonObjLine);
+            fs.appendFileSync(outputForTxt, `${newLine}\n`, (err) => {
+                if(err) {
+                    console.log("Sorry, file was not append!");
+                };
+            });
         });
     }
 
--- task1/task1.2.js
+++ task1/task1.2.js
@@ -2,4 +2,3 @@ const CSVreader = require('./src/csvreader.js');
 
 const startTaskCsvToJson = new CSVreader;
 startTaskCsvToJson.init();
-//startTaskCsvToJson.test();
