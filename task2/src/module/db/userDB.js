7b89cf8f41569b64a1b6fdd0404e6ca11c10ac99
gleb7raul <glebimptab@gmail.com>
2020-01-25 17:49:23 +0200

    feat:sb conection

---
+++ task2/src/module/db/userDB.js
@@ -0,0 +1,20 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('d1u7gj9seqre1v', 'ezdvlwqmtsvhqz', '3ebce9d1e2bccd1eb290c440038aa0faacf038b8a481de5c1c6213629a8616f7', {
+  host: 'ec2-54-247-170-5.eu-west-1.compute.amazonaws.com',
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+}
+});
+
+sequelize
+.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+module.exports = sequelize;
